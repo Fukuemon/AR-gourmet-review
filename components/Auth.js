828e09a2ca48a8925e7feb828690b78059179747
Fukuemon <Fukuemony029@gmail.com>
2023-06-05 23:06:17 +0900

    🎨 AuthコンポーネントのUI修正

--- components/Auth.js
+++ components/Auth.js
@@ -135,8 +135,8 @@ export default function Auth() {
                 className="cursor-pointer font-medium text-blue hover:text-indigo-500"
               >
                 {isLogin
-                  ? "まだアカウントをお持ちでないですか？"
-                  : "ログイン？"}
+                  ? "まだアカウントを作っていない場合はこちら"
+                  : "ログインはこちら"}
               </span>
             </div>
           </div>
