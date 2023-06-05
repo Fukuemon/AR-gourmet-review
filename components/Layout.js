d0636c56f00a87b4d49c6efb815ba9a698ca4aff
Fukuemon <Fukuemony029@gmail.com>
2023-06-05 13:53:23 +0900

    ⚡️ メインページの作成 #5
    ログイン後にページ遷移でメインページに移動する
    レイアウトコンポーネントで囲って、ログアウトのアイコンを追加

--- components/Layout.js
+++ components/Layout.js
@@ -1,136 +1,244 @@
 import Head from "next/head";
-import Link from "next/link"
+import Link from "next/link";
 
 const Navbar = ({ title }) => (
-<div className="navbar bg-base-100">
+  <div className="navbar bg-base-100">
     <div className="navbar-start flex-1">
-    <a className="btn btn-ghost normal-case text-xl">グルメモ</a>
+      <a className="btn btn-ghost normal-case text-xl">グルメモ</a>
     </div>
     <div className="navbar-center">
-    <a className="btn btn-ghost normal-case text-xl">{title}</a>
+      <a className="btn btn-ghost normal-case text-xl">{title}</a>
     </div>
     <UserProfileDropdown />
-</div>
+  </div>
 );
 
 const UserProfileDropdown = () => (
-<div className="dropdown dropdown-end">
+  <div className="dropdown dropdown-end">
     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-    <div className="w-10 rounded-full">
+      <div className="w-10 rounded-full">
         <img src="" alt="User Profile" />
-    </div>
+      </div>
     </label>
-    <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-    <li>
+    <ul
+      tabIndex={0}
+      className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+    >
+      <li>
         <a className="justify-between">Profile</a>
-    </li>
-    <li><a>Logout</a></li>
+      </li>
+      <li>
+        <a>Logout</a>
+      </li>
     </ul>
-</div>
+  </div>
 );
 
 const Main = ({ children }) => (
-<main className="flex flex-1 justify-center items-center flex-col w-screen">
+  <main className="flex flex-1 justify-center items-center flex-col w-screen">
     {children}
-</main>
+  </main>
 );
 
 const Footer = () => (
-<footer className={"w-full h-6 flex justify-center items-center border-t"}>
+  <footer className={"w-full h-6 flex justify-center items-center border-t"}>
     <BottomNavBar />
-</footer>
+  </footer>
 );
 
 const BottomNavBar = () => (
-
-<div class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
+  <div class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
     <div class="grid h-full max-w-lg grid-cols-5 mx-auto">
-    <div class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
-                <div class="grid h-full max-w-lg grid-cols-5 mx-auto">
-                    <button data-tooltip-target="tooltip-home" type="button" class="inline-flex flex-col items-center justify-center px-5 rounded-l-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
-                        <Link href="/">
-                            <a>
-                                <svg className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path></svg>
-                                <span class="sr-only">Home</span>
-                            </a>
-                        </Link>
-                    </button>
-                    <div id="tooltip-home" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                        Home
-                        <div class="tooltip-arrow" data-popper-arrow></div>
-                    </div>
+      <div class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
+        <div class="grid h-full max-w-lg grid-cols-5 mx-auto">
+          <button
+            data-tooltip-target="tooltip-home"
+            type="button"
+            class="inline-flex flex-col items-center justify-center px-5 rounded-l-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            <Link href="/main-page">
+              <a>
+                <svg
+                  className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  ></path>
+                </svg>
+                <span class="sr-only">Home</span>
+              </a>
+            </Link>
+          </button>
+          <div
+            id="tooltip-home"
+            role="tooltip"
+            class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          >
+            Home
+            <div class="tooltip-arrow" data-popper-arrow></div>
+          </div>
 
-                    <button data-tooltip-target="tooltip-home" type="button" class="inline-flex flex-col items-center justify-center px-5 rounded-l-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
-                        <Link href="/search">
-                            <a>
-                                <svg className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path></svg>
-                                <span class="sr-only">Search</span>
-                            </a>
-                        </Link>
-                    </button>
-                    <div id="tooltip-home" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                        Search
-                        <div class="tooltip-arrow" data-popper-arrow></div>
-                    </div>
+          <button
+            data-tooltip-target="tooltip-home"
+            type="button"
+            class="inline-flex flex-col items-center justify-center px-5 rounded-l-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            <Link href="/search">
+              <a>
+                <svg
+                  className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                  ></path>
+                </svg>
+                <span class="sr-only">Search</span>
+              </a>
+            </Link>
+          </button>
+          <div
+            id="tooltip-home"
+            role="tooltip"
+            class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          >
+            Search
+            <div class="tooltip-arrow" data-popper-arrow></div>
+          </div>
 
-                    <div class="flex items-center justify-center">
-                        <button data-tooltip-target="tooltip-new" type="button" class="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
-                            <Link href="/reviewpage">
-                                <a>
-                                    <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                        <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"></path>
-                                    </svg>
-                                    <span class="sr-only">New Review</span>
-                                </a>
+          <div class="flex items-center justify-center">
+            <button
+              data-tooltip-target="tooltip-new"
+              type="button"
+              class="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
+            >
+              <Link href="/reviewpage">
+                <a>
+                  <svg
+                    class="w-6 h-6 text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path
+                      clip-rule="evenodd"
+                      fill-rule="evenodd"
+                      d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                    ></path>
+                  </svg>
+                  <span class="sr-only">New Review</span>
+                </a>
+              </Link>
+            </button>
+          </div>
+          <div
+            id="tooltip-new"
+            role="tooltip"
+            class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          >
+            Create new Review
+            <div class="tooltip-arrow" data-popper-arrow></div>
+          </div>
 
-                            </Link>
-                        </button>
-                    </div>
-                    <div id="tooltip-new" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                        Create new Review
-                        <div class="tooltip-arrow" data-popper-arrow></div>
-                    </div>
+          <button
+            data-tooltip-target="tooltip-settings"
+            type="button"
+            class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            <Link href="/artestview">
+              <a>
+                <svg
+                  class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                    d="M21 7.5l-2.25-1.313M21 7.5v2.25m0-2.25l-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3l2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75l2.25-1.313M12 21.75V19.5m0 2.25l-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25"
+                  ></path>
+                </svg>
+                <span class="sr-only">ARView</span>
+              </a>
+            </Link>
+          </button>
+          <div
+            id="tooltip-settings"
+            role="tooltip"
+            class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          >
+            ARView
+            <div class="tooltip-arrow" data-popper-arrow></div>
+          </div>
 
-                    <button data-tooltip-target="tooltip-settings" type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-                        <Link href="/artestview">
-                            <a>
-                                <svg class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path clip-rule="evenodd" fill-rule="evenodd" d="M21 7.5l-2.25-1.313M21 7.5v2.25m0-2.25l-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3l2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75l2.25-1.313M12 21.75V19.5m0 2.25l-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25"></path></svg>
-                                <span class="sr-only">ARView</span>
-                            </a>
-                        </Link>
-                    </button>
-                    <div id="tooltip-settings" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                        ARView
-                        <div class="tooltip-arrow" data-popper-arrow></div>
-                    </div>
-
-                    <button data-tooltip-target="tooltip-profile" type="button" class="inline-flex flex-col items-center justify-center px-5 rounded-r-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
-                        <svg class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path clip-rule="evenodd" fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"></path>
-                        </svg>
-                        <span class="sr-only">Profile</span>
-                    </button>
-                    <div id="tooltip-profile" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                        Profile
-                        <div class="tooltip-arrow" data-popper-arrow></div>
-                    </div>
-                </div>
-            </div>
+          <button
+            data-tooltip-target="tooltip-profile"
+            type="button"
+            class="inline-flex flex-col items-center justify-center px-5 rounded-r-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            <svg
+              class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+              ></path>
+            </svg>
+            <span class="sr-only">Profile</span>
+          </button>
+          <div
+            id="tooltip-profile"
+            role="tooltip"
+            class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          >
+            Profile
+            <div class="tooltip-arrow" data-popper-arrow></div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 );
 
-export default function Layout({ children, title = "グルグラ by Nextjs", showHeader = true, showFooter = true }) {
-    return (
+export default function Layout({
+  children,
+  title = "グルグラ by Nextjs",
+  showHeader = true,
+  showFooter = true,
+}) {
+  return (
     <div>
-        <Head>
+      <Head>
         <title>{title}</title>
-        </Head>
-        {showHeader && <Navbar title={title}/>}
-        <Main>
-        {children}
-        </Main>
-        {showFooter && <Footer />}
+      </Head>
+      {showHeader && <Navbar title={title} />}
+      <Main>{children}</Main>
+      {showFooter && <Footer />}
     </div>
-    );
+  );
 }
