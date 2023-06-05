4089c2dbd16b56f61f2f1904d344b1e39e888b77
Fukuemon <Fukuemony029@gmail.com>
2023-06-05 13:52:50 +0900

    ⚡️ APIエンドポイントとトークンの取り扱いを修正し、UIの一部を調整 #5
    - レスポンスエラーのハンドリング改善：
    不正なステータスコードを受け取った場合にエラーをスローするように
    
    - アクセストークンの格納方式を改善：
     CookieにJWTを加えて格納するように変更
    
    - レスポンスデータのハンドリングを改善：
     `access`プロパティの存在確認と、不正なデータに対するエラーのスローを追加しました。
    
    - UIの語彙を調整：
    "サインアップ"を"アカウント作成"に変更。また、誤ったパスワード入力時の挙動も修正
    
    - その他のフォームのスタイルと語彙を微調整

--- components/Auth.js
+++ components/Auth.js
@@ -1,149 +1,156 @@
-import { LockClosedIcon } from '@heroicons/react/solid'
-import { useRouter } from 'next/router'
-import { useState } from 'react'
-import Cookie from "universal-cookie"
+import { LockClosedIcon } from "@heroicons/react/solid";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Cookie from "universal-cookie";
 
 const cookie = new Cookie();
 
 export default function Auth() {
-    const router = useRouter();
+  const router = useRouter();
 
-    // useState フック：　email, password, isLogin の状態を管理
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [isLogin, setIsLogin] = useState(true);
+  // useState フック：　email, password, isLogin の状態を管理
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+  // ログインの非同期関数を定義
+  const login = async () => {
+    try {
+      // fetch を用いて、指定された API エンドポイントに POST リクエストを送信
+      await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}authen/jwt/create/`, {
+        method: "POST",
+        body: JSON.stringify({ email: email, password: password }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        //fetchの結果をresで受け取る
+        .then((res) => {
+          if (res.status === 400) {
+            //認証に失敗した場合
+            throw "authentication failed"; //エラーを返す
+          } else if (res.ok) {
+            //成功した場合
+            return res.json(); //JSONオブジェクトに変換して返す
+          } else {
+            throw new Error(`Unexpected response: ${res.statusText}`);
+          }
+        })
+        //cookieにアクセストークンを格納うする
+        .then((data) => {
+          if (data && "access" in data) {
+            const options = { path: "/" };
+            cookie.set("access_token", `JWT ${data.access}`, options); //ルートのpath以下でcookieが使える
+          } else {
+            throw new Error("Invalid login data");
+          }
+        });
+      router.push("/main-page"); //成功した場合main-pageに移動する
+    } catch (err) {
+      alert(err);
+    }
+  };
 
-    // ログインの非同期関数を定義
-    const login = async () => {
-        try {
-            // fetch を用いて、指定された API エンドポイントに POST リクエストを送信
-            await fetch(
-                `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`,
-                {
-                    method: "POST",
-                    body: JSON.stringify({ email: email, password: password}),
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            )
-            //fetchの結果をresで受け取る
-            .then((res) => {
-                if (res.status === 400) { //認証に失敗した場合
-                    throw "authentication failed"; //エラーを返す
-                } else if (res.ok) { //成功した場合
-                    return res.json(); //JSONオブジェクトに変換して返す
-                }
-            })
-            //cookieにアクセストークンを格納うする
-            .then((data) => {
-                const options = { path: "/" };
-                cookie.set("access_token", data.access, options); //ルートのpath以下でcookieが使える
-            });
-            router.path("/main-page") //成功した場合main-pageに移動する
-        } catch (err) {
-            alert(err);
-        }
-        };
+  // email を用いた認証の非同期関数
+  const authEmail = async (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      //ログインの場合
+      login();
+    } else {
+      try {
+        //registerAPIで新規アカウントを作成する
+        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register`, {
+          method: "POST",
+          body: JSON.stringify({ email: email, password: password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          }
+        });
+        //作成したら、そのままログインする
+        login();
+      } catch (err) {
+        alert(err);
+      }
+    }
+  };
 
-    // email を用いた認証の非同期関数
-    const authEmail = async (e) => {
-        e.preventDefault();
-        if (isLogin) { //ログインの場合
-            login();
-        } else { //そうでない場合
-            try{
-            //registerAPIで新規アカウントを作成する
-            await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register/`, { 
-                method: "POST",
-                body: JSON.stringify({ email: email, password: password }),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }).then((res) => {
-                if (res.status === 400) {
-                    throw "authentication failed";
-                }
-            });
-            //作成したら、そのままログインする
-            login();
-            } catch (err) {
-                alert(err);
-            }
-        }
-    };
-    
-
-return (
+  return (
     <div className="flex flex-col h-screen items-center justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt=""
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt=""
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            {/* 　ログインか新規作成かを判断 */}
-            {isLogin ? "ログイン" : "サインアップ"}
+          {/* 　ログインか新規作成かを判断 */}
+          {isLogin ? "ログイン" : "アカウント作成"}
         </h2>
-        </div>
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            {/* onSubmitでauthEmail関数を実行するようにする */}
-        <form className="space-y-6" action="#" method="POST" onSubmit={authEmail}>
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        {/* onSubmitでauthEmail関数を実行するようにする */}
+        <form className="mt-8 space-y-6" onSubmit={authEmail}>
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
             <div>
-            <div className="mt-2">
-                <input
+              <input
                 name="email"
                 type="email"
                 autoComplete="email"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder='Emailを入力'
+                placeholder="Emailを入力"
                 value={email}
                 onChange={(e) => {
-                    setEmail(e.target.value);
+                  setEmail(e.target.value);
                 }}
-                />
+              />
             </div>
-            </div>
-
             <div>
-            <div className="mt-2">
-                <input
+              <input
                 name="password"
                 type="password"
                 autoComplete="current-password"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder='パスワードを入力'
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="パスワードを入力"
                 value={password}
                 onChange={(e) => {
-                    setEmail(e.target.value)
+                  setPassword(e.target.value);
                 }}
-                
-                />
+              />
             </div>
-            </div>
+          </div>
 
-            <div className="flex items-center justify-center">
-                <div className="text-sm">
-                    {/* クリックするとisLoginの状態が反転する */}
-                    <span onClick={() => setIsLogin(!isLogin)} className='cursor-pointer font-medium text-white hover:text-indigo-500'>
-                        {isLogin ? "ログイン？" : "サインアップ？"}
-                    </span>
-                </div>
+          <div className="flex items-center justify-center">
+            <div className="text-sm">
+              {/* クリックするとisLoginの状態が反転する */}
+              <span
+                onClick={() => setIsLogin(!isLogin)}
+                className="cursor-pointer font-medium text-blue hover:text-indigo-500"
+              >
+                {isLogin
+                  ? "まだアカウントをお持ちでないですか？"
+                  : "ログイン？"}
+              </span>
             </div>
+          </div>
 
-            <div>
+          <div>
             <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                {isLogin ? "ログイン" : "新規作成"}
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              {isLogin ? "ログイン" : "新規作成"}
             </button>
-            </div>
+          </div>
         </form>
-        </div>
+      </div>
     </div>
-)
+  );
 }
